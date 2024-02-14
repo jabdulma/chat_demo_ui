233055f3b12c724bbc0d8a5024469f8c09b0ef4c
John Abdul-Masih <jabdulma@gmail.com>
2024-02-14 15:22:13 -0600

    Adding azure api. Package.json update for azure

---
+++ azureapi/talktoai/index.js
@@ -0,0 +1,17 @@
+import OpenAI from "openai";
+import 'dotenv/config';
+import express from "express";
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
